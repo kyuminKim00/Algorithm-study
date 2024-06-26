8b933ed94a887388bbba58bcfe239a784f182e2d
kyuminKim00 <kkm8121@naver.com>
2024-06-26 17:49:48 +0900

    24-06-26

--- ShortestPath.py
+++ ShortestPath.py
@@ -81,3 +81,166 @@
 # 출발 노드로부터 가장 거리가 짧은 노드를 더욱 빠르게 찾을 수 있다.
 # 힙 자료구조 중 최소 힙은 값이 가장 낮은 원소를 먼저 추출하는데 단순히 힙을 이용해 만든 우선순위 큐를 이용해
 # 시작 노드로부터 거리가 짧은 노드 순서대로 큐에서 나올 수 있도록 다익스트라 알고리즘을 작성한다.
+
+# import heapq
+# import sys
+# input = sys.stdin.readline
+# INF = int(1e9)
+
+# n, m = map(int, input().split()) # 노드의 개수, 간선의 개수
+# start = int(input()) # 시작 노드 번호
+# graph = [[] for i in range(n+1)] # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트 만들기
+# distance = [INF] * (n+1) # 최단 거리 테이블 초기화
+
+# for _ in range(m): # 간선 정보(비용) 입력
+#     a, b, c = map(int, input().split())
+#     graph[a].append((b, c))
+    
+# def dijkstra(start):
+#     q = []
+#     heapq.heappush(q, (0, start)) # 거리 0, 시작 노드, 우선순위 큐에 삽입
+#     distance[start] = 0 # 시작 노드의 거리는 0
+#     while q: # 큐가 비어 있지 않다면 계속 실행
+#         dist, now = heapq.heappop(q) # 가장 최단 거리가 짧은 노드 꺼내기
+#         if distance[now] < dist: # 현재꺼낸 노드가 이미 처리된 적이 있는 노드라면 무시
+#             continue
+#         for i in graph[now]: # 현재 노드에 연결된 노드 확인
+#             cost = dist + i[1]
+#             if cost < distance[i[0]]: # 현재노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+#                 distance[i[0]] = cost
+#                 heapq.heappush(q, (cost, i[0]))
+
+        
+# dijkstra(start)
+
+# for i in range(1, n+1):
+#     if distance[i] == INF:
+#         print("INF")
+#     else:
+#         print(distance[i])
+
+# 위의 개선된 다익스트라 알고리즘 구현은 간단한 다익스트라 알고리즘에 비해 시간복잡도가 O(ElogV)로 빠름.
+
+
+### 플로이드 워셜 알고리즘 ###
+# 다익스트라는 '한 지점'에서 다른 지점까지의 최단 경로를 구할 때 사용하는 알고리즘이라면
+# 플로이드 워셜은 '모든 지점'에서 다른 모든 지점까지의 최단 경로를 모두 구해야 하는 경우에 사용하는 알고리즘.
+# 플로이드 워셜 알고리즘의 시간 복잡도 O(N^3)
+# 다익스트라에서는 출발 노드가 1개이므로 최단 거리를 저장하기 위해 1차원 리스트를 사용하지만
+# 플로이드 워셜에서는 2차원 리스트에 최단 거리 정보를 저장.
+# 다익스트라는 그리디 알고리즘이지만 플로이드 워셜은 다이나믹 프로그래밍이다.
+
+# 점화식 : Dab = min(Dab, Dak + Dkb)
+# A에서 B로 가는 최소 비용과 A에서 K를 거쳐 B로 가는 비용을 비교하여 더 작은 값으로 갱신
+
+# INF = int(1e9)
+# n = int(input()) # 노드 개수
+# m = int(input()) # 간선 개수
+# graph = [[INF] * (n+1) for _ in range(n+1)] # 노드끼리의 거리를 담는 2차원 리스트, 이어져있지 않으면 INF
+
+# for a in range(1, n+1):
+#     for b in range(1, n+1):
+#         if a == b:
+#             graph[a][b] = 0 # 자기 자신에서 자기 자신으로 가는 비용은 0
+
+# for _ in range(m): # 각 간선에 대한 정보 입력, 초기화
+#     a, b, c = map(int, input().split())
+#     graph[a][b] = c
+    
+# for k in range(1, n+1):
+#     for a in range(1, n+1):
+#         for b in range(1, n+1):
+#             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b]) # 점화식에 따라 구현
+            
+# for a in range(1, n+1):
+#     for b in range(1, n+1):
+#         if graph[a][b] == INF:
+#             print("INF")
+#         else:
+#             print(graph[a][b], end = " ")
+#     print()
+
+
+## 실전문제2 : 미래도시 ##
+# 플로이드 워셜 알고리즘 문제 #
+
+# n, m = map(int, input().split())
+# INF = int(1e9)
+# graph = [[INF] * (n+1) for _ in range(n+1)]
+
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         if i == j:
+#             graph[i][j] = 0
+            
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a][b] = 1
+#     graph[b][a] = 1
+    
+# x, k = map(int,input().split())
+
+# for m in range(1, n+1):
+#     for i in range(1, n+1):
+#         for j in range(1, n+1):
+#             cost = graph[i][j]
+#             d_cost = graph[i][m] + graph[m][j]
+#             if d_cost < cost:
+#                 graph[i][j] = d_cost
+
+
+
+# k_cost = graph[1][k]
+# x_cost = graph[k][x]
+# cost = k_cost + x_cost
+# if cost >= INF:
+#     print(-1)
+# else:
+#     print(cost)
+
+## 실전문제3 : 전보 ##
+# 다익스트라 알고리즘 문제#
+
+# import heapq
+# import sys
+# input = sys.stdin.readline
+# INF = int(1e9)
+
+# n, m, start = map(int,input().split())
+
+# graph = [[] for _ in range(n+1)]
+# distance = [INF] * (n+1)
+
+# for _ in range(m):
+#     a, b, c = map(int,input().split())
+#     graph[a].append((b, c))
+    
+# def dijkstra(start):
+#     q = []
+#     distance[start] = 0
+#     heapq.heappush(q, (0, start))
+    
+#     while q:
+#         dist, now = heapq.heappop(q)
+#         if distance[now] < dist:
+#             continue
+        
+#         for i in graph[now]:
+#             cost = dist + i[1]
+#             if cost < distance[i[0]]:
+#                 distance[i[0]] = cost
+#                 heapq.heappush(q, (cost, i[0]))
+                
+# dijkstra(start)
+# count = 0
+# max_distance = 0
+# for d in distance:
+#     if d < INF:
+#         count += 1
+#         max_distance = max(max_distance, d)
+        
+# print(count-1, max_distance)
+    
+        
+
+
